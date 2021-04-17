52e8ac74e554723e77239716857ad2b4fe356e7a
Eden Johnson <50639814+edenjohnson@users.noreply.github.com>
2021-04-17 15:32:00 -0700

    Update protein_HMM_script.py

--- protein_HMM_script.py
+++ protein_HMM_script.py
@@ -2,12 +2,12 @@
 import os
 
 # Call the different Python files for each step of the program
-blast = "python BLAST.py"
-curate_BLAST_Results = "python curate_BLAST_results.py"
-msa = "python MSA.py"
-adjust_fasta = "python adjust_fasta_files.py"
-phylogenetic_tree = "python phylogenetic_tree.py"
-build_HMM = "python build_HMM.py"
+blast = "python3 BLAST.py"
+curate_BLAST_Results = "python3 curate_BLAST_results.py"
+msa = "python3 MSA.py"
+adjust_fasta = "python3 adjust_fasta_files.py"
+phylogenetic_tree = "python3 phylogenetic_tree.py"
+build_HMM = "python3 build_HMM.py"
 
 os.system(blast)
 os.system(curate_BLAST_Results)
